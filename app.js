33967fe28575823ab7803ce7c7e4627fb79f4d8e
swastikpatro <patroswastik.jsdev@gmail.com>
2022-09-21 14:24:28 +0530

    refreactor: selected all elements

--- app.js
+++ app.js
@@ -1,1 +1,7 @@
 console.log('Hare Krishna');
+
+const bill = document.getElementById('bill-input');
+const customerGave = document.getElementById('customer-input');
+const btnContainer = document.querySelector('.btn-container');
+const alertText = document.querySelector('.alert');
+const table = document.querySelector('.table');
