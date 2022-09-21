7e0e6ac351eaead018853cb2aef4b583d3c34d8e
swastikpatro <patroswastik.jsdev@gmail.com>
2022-09-21 14:25:52 +0530

    feat: added alertMsg() which shows alert

--- app.js
+++ app.js
@@ -5,3 +5,17 @@ const customerGave = document.getElementById('customer-input');
 const btnContainer = document.querySelector('.btn-container');
 const alertText = document.querySelector('.alert');
 const table = document.querySelector('.table');
+
+function alertMsg(type, msg) {
+  const tID = setInterval(() => {
+    alertText.innerText = msg;
+    alertText.classList.add(`alert-${type}`);
+    alertText.classList.add('show-alert');
+  }, 0);
+
+  setTimeout(() => {
+    clearInterval(tID);
+    alertText.classList.remove(`alert-${type}`);
+    alertText.classList.remove('show-alert');
+  }, 1000);
+}
