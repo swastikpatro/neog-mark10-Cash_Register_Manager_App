a497d44b3094aedc679a2911dedc6fbae66f28f5
swastikpatro <patroswastik.jsdev@gmail.com>
2022-09-21 14:27:08 +0530

    feat: added notesArr and defaultData obj

--- app.js
+++ app.js
@@ -19,3 +19,11 @@ function alertMsg(type, msg) {
     alertText.classList.remove('show-alert');
   }, 1000);
 }
+
+const notesArr = [2000, 500, 100, 50, 20, 10, 5, 2, 1];
+
+const defaultData = notesArr.reduce((acc, curr) => {
+  acc[curr] = '-';
+  return acc;
+}, {});
+console.log(defaultData);
